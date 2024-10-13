b803cc9dfac2919bae64f104927f8ce4e09ded15
huutai <huutt201@example.com>
2024-10-13 10:27:53 +0700

    app.use(

--- src/main.ts
+++ src/main.ts
@@ -32,6 +32,24 @@ async function bootstrap() {
   // handle global exception
   app.useGlobalFilters(new AllExceptionsFilter());
 
+  app.use(
+    (
+      req: any,
+      res: { header: (arg0: string, arg1: string) => void },
+      next: () => void,
+    ) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+      next();
+    },
+  );
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Life manager documents')
     .setDescription('The life manager API description')
