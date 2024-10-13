8ab6dfb06c548f1880ad388c7d6019789a8a089d
huutai <huutt201@example.com>
2024-10-13 10:34:24 +0700

    enableCors

--- src/main.ts
+++ src/main.ts
@@ -8,32 +8,27 @@ import * as http from 'http';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './exception/all-exception.filter';
 
-// const corsOptions: CorsOptions = {
-//   origin: '*',
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   allowedHeaders:
-//     'Content-Type, Authorization, Content-Length, X-Requested-With, Accept, Accept-Versioning, Origin, Access-Control-Request-Origin, Access-Control-Request-Headers, Access-Control-Request-Method',
-//   exposedHeaders:
-//     'Content-Type, Authorization, Content-Length, X-Requested-With, Accept, Accept-Versioning, Origin, Access-Control-Request-Origin, Access-Control-Request-Headers, Access-Control-Request-Method',
-//   maxAge: 1728000,
-// };
+const corsOptions: CorsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders:
+    'Content-Type, Authorization, Content-Length, X-Requested-With, Accept, Accept-Versioning, Origin, Access-Control-Request-Origin, Access-Control-Request-Headers, Access-Control-Request-Method',
+  exposedHeaders:
+    'Content-Type, Authorization, Content-Length, X-Requested-With, Accept, Accept-Versioning, Origin, Access-Control-Request-Origin, Access-Control-Request-Headers, Access-Control-Request-Method',
+  maxAge: 1728000,
+};
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<string>('PORT');
   app.use(json({ limit: '10mb' }));
   app.use(urlencoded({ extended: true, limit: '10mb' }));
-
+  app.enableCors(corsOptions);
   // transform data to DTO
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   // handle global exception
   app.useGlobalFilters(new AllExceptionsFilter());
-
-  app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
-  });
 
   const config = new DocumentBuilder()
     .setTitle('Life manager documents')
