75a1372b2dfb8f4ae598fc67c05e4b33c0739e5b
huutai <huutt201@example.com>
2025-05-17 09:20:37 +0700

    add location to tag

--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UserRepository } from 'src/repositories';
 import { PostRepository } from 'src/repositories/blog.repository';
+import { LocationRepository } from 'src/repositories/location.repository';
 import { TypeOrmExModule } from 'src/typeorm';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
 
 @Module({
   imports: [
-    TypeOrmExModule.forCustomRepository([PostRepository, UserRepository]),
+    TypeOrmExModule.forCustomRepository([
+      PostRepository,
+      UserRepository,
+      LocationRepository,
+    ]),
   ],
   providers: [PostService],
   controllers: [PostController],
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -4,6 +4,7 @@ import { PostEntity } from 'src/entities/post.entity';
 import { coreHelper } from 'src/helpers';
 import { UserRepository } from 'src/repositories';
 import { PostRepository } from 'src/repositories/blog.repository';
+import { LocationRepository } from 'src/repositories/location.repository';
 import { In } from 'typeorm';
 
 @Injectable()
@@ -11,6 +12,7 @@ export class PostService {
   constructor(
     private readonly repo: PostRepository,
     private readonly userRepo: UserRepository,
+    private readonly locationRepo: LocationRepository,
   ) {}
   async create(blog: Partial<PostEntity>): Promise<PostEntity> {
     return this.repo.save(blog);
@@ -28,7 +30,7 @@ export class PostService {
 
     for (const item of res[0]) {
       item.imgs = item.imgs.split(',');
-      item.tag = item.tag.split(',');
+      item.tag = item.tag.trim().split(',');
     }
 
     const userIds = res[0].map((item: any) => item.createdBy);
@@ -42,6 +44,31 @@ export class PostService {
     for (const item of res[0]) {
       item.user = dictUserById[item.createdBy];
       item.username = dictUserById[item.createdBy].username;
+    }
+
+    const allLabels = res[0]
+      .map((item: any) => item.tag)
+      .flat()
+      .filter((item: any) => item !== '');
+
+    const locationsByLabels = await this.locationRepo.find({
+      where: {
+        label: In(allLabels),
+        isDeleted: false,
+      },
+    });
+    const dictLocationsByLabel: any = coreHelper.toDict(
+      locationsByLabels,
+      'label',
+    );
+    for (const item of res[0]) {
+      const locations: any = [];
+      for (const label of item.tag) {
+        if (dictLocationsByLabel[label]) {
+          locations.push(dictLocationsByLabel[label]);
+        }
+      }
+      item.locations = locations;
     }
 
     return {
